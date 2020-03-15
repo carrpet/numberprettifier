ad16eb58d9af5cf2c036ae227a1d0defbfb0eaca
Peter Carr <carrpet@gmail.com>
2020-03-15 13:30:14 -0600

    hashmap improvements and comments

--- numberprettifier.py
+++ numberprettifier.py
@@ -1,10 +1,14 @@
-from enum import IntEnum
+from typing import Tuple
 
 class PrettyNumber:
    
-    def __init__(self,data):
+    def __init__(self, data: int):
         self._data = str(data)
-        self._scaleDict = {k:v for (k,v) in [(2,'M'),(3,'B'),(4,'T')]}
+        def digitsToScale(i):
+            return (i - 1) // 3
+        # a dictionary mapping the number of digits to the scale factor 
+        self._scaleDict = {(i,'M') if digitsToScale(i) == 2 else (i,'B') if digitsToScale(i) == 33 else (i,'T') for i in range(7,16)}
+        
 
     def _countDigits(self) -> int:
         dpIndex = self._data.find(".")
@@ -13,33 +17,40 @@ class PrettyNumber:
         else:
             return dpIndex
     
-    def _round(self,leading,toRound,roundOn):
+    # _round returns a tuple of the digits to be printed before the decimal,
+    # the digit to be printed after the decimal, and a bool indicating whether
+    # the number overflowed to the next magnitude 
+    def _round(self, leading: int, toRound: int ,roundOn: int) -> Tuple[str,str,bool]:
         if roundOn >= 5:
             rounded2 = (toRound + 1) % 10
+            # if toRound overflowed then also increment the number to the left
             if rounded2 < toRound:
                 rounded1 = (leading + 1) % 1000
                 rounded1 = 1 if rounded1 == 0 else rounded1 
-                return (str(rounded1),str(rounded2))
-            return (str(leading),str(rounded2))
-        return (str(leading), str(toRound))
+                return (str(rounded1),str(rounded2),True)
+            return (str(leading),str(rounded2), False)
+        return (str(leading), str(toRound), False)
 
-    def _truncate(self):
+    def _truncate(self) -> str:
         numDigits = self._countDigits()
-        scaleDigitHash = (numDigits -1) // 3
-        if scaleDigitHash in self._scaleDict:
+        if numDigits in self._scaleDict:
             leadingDigits = 3 if numDigits % 3 == 0 else numDigits % 3
             first = int(self._data[0:leadingDigits])
             toRound = int(self._data[leadingDigits])
             roundOn = int(self._data[leadingDigits + 1])
             rounded = self._round(first,toRound,roundOn)
+            # if the number of digits is no longer the same
+            # then we must have overflowed so increment the total number
+            # of digits
             if len(rounded[0]) != len(str(first)):
                 numDigits += 1
-                scaleDigitHash = numDigits // 3
-            if scaleDigitHash in self._scaleDict:
-                if rounded[1] == '0':
-                    return '{}{}'.format(rounded[0],self._scaleDict[scaleDigitHash])
+            if numDigits in self._scaleDict:
+                # since the number of digits overflowed it must be an integer so 
+                # truncate the decimal approximation
+                if rounded[1]:
+                    return '{}{}'.format(rounded[0],self._scaleDict[numDigits])
                 else:
-                    return '{}.{}{}'.format(rounded[0],rounded[1],self._scaleDict[scaleDigitHash])
+                    return '{}.{}{}'.format(rounded[0],rounded[1],self._scaleDict[numDigits])
         return self._data
     
     def __repr__(self):
